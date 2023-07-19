69e1952ec483efbf4b6feee9a76bde26abd3af79
G.S <131451577+st-doval17@users.noreply.github.com>
2023-07-19 17:56:41 +0200

    Render Data in Components
    -replace hardcoded movie array with array from myFlix API in main-view.jsx
    -import PropTypes from package to movie-card.jsx
    -use PopTypes for updated components

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -7,4 +9,21 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       {movie.title}
     </div>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    image: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    director: PropTypes.shape({
+      name: PropTypes.string,
+      bio: PropTypes.string,
+    }).isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string,
+      description: PropTypes.string,
+    }),
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -9,16 +9,20 @@ export const MovieView = ({ movie, onBackClick }) => {
         <span>{movie.title}</span>
       </div>
       <div>
+        <span>Description: </span>
+        <span>{movie.description}</span>
+      </div>
+      <div>
         <span>Director: </span>
-        <span>{movie.director}</span>
+        <span>{movie.director.Name}</span>
+        <br></br>
+        <span>{movie.director.Bio}</span>
       </div>
       <div>
         <span>Genre: </span>
-        <span>{movie.genre}</span>
-      </div>
-      <div>
-        <span>Description: </span>
-        <span>{movie.description}</span>
+        <span>{movie.genre.Name}</span>
+        <br></br>
+        <span>{movie.genre.Description}</span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
