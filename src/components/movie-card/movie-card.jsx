57f4a9f9881db4c4dbedb58213ce7d9b11028b21
G.S <131451577+st-doval17@users.noreply.github.com>
2023-08-02 15:45:53 +0200

    Movie-Card Component
    - add open button to card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,15 +3,21 @@ import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie, onAddFavorite }) => {
+  const handleAddClick = () => {
+    if (onAddFavorite) {
+      onAddFavorite(movie._id);
+    }
+  };
+
   return (
     <Card className="h-100">
       <Card.Img className="flex-grow-1" variant="top" src={movie.ImagePath} />
       <Card.Body className="p-3 bg-white text-dark">
         <Card.Title>{movie.Title}</Card.Title>
         <Card.Text>{movie.Director.Name}</Card.Text>
-        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
-          <Button variant="link">Open</Button>
+        <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+          <Button>Open</Button>
         </Link>
       </Card.Body>
     </Card>
